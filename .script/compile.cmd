b670577985571e94e1e17097aff043c64b0c521e
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2023-08-29 18:08:27 +0900

    Do not compress compiled script

--- .script/compile.cmd
+++ .script/compile.cmd
@@ -2,16 +2,16 @@
 title AutoHotkey batch compile
 set "c=C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
 REM BlueStacksMultiClick
-"%c%" /in ..\BlueStacksMultiClick\BlueStacksMultiClick.ahk /cp 65001 /compress 2 /silent verbose
+"%c%" /in ..\BlueStacksMultiClick\BlueStacksMultiClick.ahk /cp 65001 /compress 0 /silent verbose
 move /Y ..\BlueStacksMultiClick\BlueStacksMultiClick.exe .
 REM MCAutoClicker
-"%c%" /in ..\MCAutoClicker\MCAutoClicker.ahk /cp 65001 /compress 2 /silent verbose
+"%c%" /in ..\MCAutoClicker\MCAutoClicker.ahk /cp 65001 /compress 0 /silent verbose
 move /Y ..\MCAutoClicker\MCAutoClicker.exe .
 REM QuickTextInput
-"%c%" /in ..\QuickTextInput\QuickTextInput.ahk /cp 65001 /compress 2 /silent verbose
+"%c%" /in ..\QuickTextInput\QuickTextInput.ahk /cp 65001 /compress 0 /silent verbose
 move /Y ..\QuickTextInput\QuickTextInput.exe .
 REM TistoryEditorHelper
-"%c%" /in ..\TistoryEditorHelper\TistoryEditorHelper.ahk /cp 65001 /compress 2 /silent verbose
+"%c%" /in ..\TistoryEditorHelper\TistoryEditorHelper.ahk /cp 65001 /compress 0 /silent verbose
 move /Y ..\TistoryEditorHelper\TistoryEditorHelper.exe .
 REM
 pause
